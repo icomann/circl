27516e373ff28b5626b70a95ba2dd18ba209da28
icomann <nicolas.mann@sansano.usm.cl>
2015-12-10 14:10:28 -0300

    Create mapinfo.py

---
+++ mapinfo.py
@@ -0,0 +1,6 @@
+class ssyst:
+    def __init__(self, file):
+        self.radius = 4096
+        ## planet: [((x,y), radius, force, (randomdropinfo), color/text), ...]
+        ##randdrinf: (((nuthin, 40), (ubergun,10), (candy,50)), period)
+        self.planets = [((90,1024), 512, 20, (0,1), 0), ((180, 2048), 512, 40, (0,2), 0)]
