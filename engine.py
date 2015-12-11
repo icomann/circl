9c86e3a7e6fc3dc3100ccbf879a1814f793625e0
icomann <nicolas.mann@sansano.usm.cl>
2015-12-11 13:27:34 -0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,2 +1,31 @@
 print "Loading Physics"
 import phys
+
+MAXHP = 100
+
+class gun:
+    ammo = 1
+    dmg = 1
+    msPeriod = 1000
+    def __init__(self, gundata=0):
+        return self
+
+fist = gun()
+fist.ammo = 5
+fist.msPeriod=0.300
+
+class shooter:
+    def __init__(self):
+        self.phsobj = phys.physicsobj()
+        self.theta = 0
+        self.invis = True
+        self.pos = phys.vector(0,0)
+        self.hp = MAXHP
+        self.active = False
+        return self
+    def respawn(self,pos,t):
+        self.hp = MAXHP
+        self.active = True
+        self.pos = pos
+        self.theta = t
+        self.invul = False
