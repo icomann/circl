ae1e84716edec0c5e8311ff9d24eac6059e23133
icomann <nicolas.mann@sansano.usm.cl>
2015-12-17 00:15:25 +0000

    vector sum and dot implemented

--- phys.py
+++ phys.py
@@ -1,5 +1,4 @@
 import math
-import time
 
 def rCAng(ang):
     return math.pi*ang/180
@@ -13,17 +12,18 @@ def mDist(pos1, pos2):
 class vector:
     def __init__(self, x=0, y=0, r=1):
         self.x,self.y = r*x, r*y
-    def suma(self,ex,y='vector'):
-        if y == 'vector':
-            self.x += ex.x
-            self.y += ex.y
-            return
-        self.x+=ex
-        self.y+=y
-    def mult(self,r):
-        self.x*=r
-        self.y*=r
-    def diag(self):
+
+    def __sum__(self,vect):
+        if y is not vector:
+            return self
+        return vector(self.x+vect.x, self.y+vect.y)
+
+    def __mul__(self,r):
+        if r is not vector:
+            return vector(self.x*r, self.y*r)
+	return self.x*r.x+self.y+r.y
+        
+    def dir(self):
         mag = mDist(self,vector(0,0))
         self.x /= mag
         self.y /= mag
@@ -37,18 +37,14 @@ class polarC:
         self.ang = ang
         
 class physicsobj:
-    def __init__(self, mass):
-        self.mass = mass
-        self.a = vector(0,0)
-        self.v = vector(0,0)
+    def __init__(self, vel=vector(0,0), acc=vector(0,0)):
+        self.a = acc
+        self.v = vel
     def tick(self,deltat,vNetforce=vector(0,0)):
-        self.a.suma(vNetforce.mult(1.0/mass))
-        self.v.suma(a.mult(deltat))
+        self.a += vNetforce
+        self.v += self.a*deltat
     def movement(self,deltat):
-        mov = vector()
-        mov.suma(self.v.mult(deltat))
-        mov.suma(self.a.mult(deltat*deltat*0.5))
-        return mov
+        return v*deltat+a*(deltat*deltat/2)
 
 def rAng(pos1, pos2):
     return atan(float(pos1.y)-pos2.y/pos1.x-pos2.x)
