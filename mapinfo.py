f628bb4bb3c0c23d80b99ee9040189742f8970af
icomann <nicolas.mann@sansano.usm.cl>
2015-12-22 12:26:55 -0300

    Update mapinfo.py

--- mapinfo.py
+++ mapinfo.py
@@ -1,13 +1,15 @@
 from phys import vector
+
 class planet:
     def __init__(self, dataarray):
-        S, x, y, r, f, fr, i, c = dataarray
+        S, x, y, r, f, fr, i, t, c = dataarray
         self.pos = vector(x,y)
         self.spawn = (S=='*')
         self.radius = r
         self.fradius = fr
         self.force = f
         self.items = i
+        self.period = t
         self.color = tuple(c.split('-')
 
 class ssyst:
@@ -16,9 +18,21 @@ class ssyst:
         self.title = 'unnamed'
         self.author 'unknown'
         self.pl = (1,99)
+        self.planets = list()
         file = open(file)
         for line in file:
             line = line.strip()
+            if line[0] == '-':
+                if line[1] == 'r':
+                    self.radius = int(line[2:])
+                elif line[1] == 'p':
+                    self.pl = tuple(line[2:].split(','))
+                elif line[1] == 'a':
+                    self.author = line[2:0]
+                elif line[1] == 't':
+                    self.title = line[2:0]
+            continue
         ## planet: [((x,y), radius, force, (randomdropinfo), color/text), ...] hopeigettoletyouknow
         ##randdrinf: (((nuthin, 40), (ubergun,10), (candy,50)), period)        beforetheylockmesillyarseaway
-        self.planets = [((90,1024), 512, 20, (0,1), 0), ((180, 2048), 512, 40, (0,2), 0)]
+            line = line.split(';')
+            self.planets.append(line)
