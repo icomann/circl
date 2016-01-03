8e34aa413f4aad7d8b94ca1c2ac612f932fe8255
icomann <nicolas.mann@sansano.usm.cl>
2016-01-03 05:23:59 +0000

    getting things visible

--- circl.py
+++ circl.py
@@ -1,4 +1,18 @@
+#!/bin/python2
+
 print "Loading engine"
 import engine
-print "Loading loader... yep"
+print "Loading maps"
 import mapinfo
+print "Loading languages"
+import locale
+print "Loading pygame"
+import pygame
+
+while True:
+    mp = int(raw_input('Choose Map> '))
+    if mp >= 0 and mp < len(mapinfo.ssyslist):
+        mp = mapinfo.ssyslist[mp]
+        break
+
+print mp
--- mapinfo.py
+++ mapinfo.py
@@ -1,41 +1,54 @@
 from phys import vector
+from os import listdir
 
 class planet:
     def __init__(self, dataarray):
         S, x, y, r, f, fr, i, t, c = dataarray
-        self.pos = vector(x,y)
+        self.vPos = vector(x,y)
         self.spawn = (S=='*')
-        self.radius = r
-        self.fradius = fr
-        self.force = f
+        self.radius = int(r)
+        self.fradius = int(fr)
+        self.force = int(f)
         self.items = i
-        self.period = t
-        self.color = tuple(c.split('-')
+        self.period = int(t)
+        self.color = tuple(c.split('-'))
 
 class ssyst:
     def __init__(self, file):
         self.radius = 4096
         self.title = 'unnamed'
-        self.author 'unknown'
+        self.author = 'unknown'
         self.pl = (1,99)
         self.planets = list()
         file = open(file)
         for line in file:
             line = line.strip()
-            if line[0] == '-':
+            if line[0] == ':':
                 if line[1] == 'r':
                     self.radius = int(line[2:])
                 elif line[1] == 'p':
-                    self.pl = tuple(line[2:].split(','))
+                    self.pl = tuple(map(int,line[2:].split(',')))
                 elif line[1] == 'a':
-                    self.author = line[2:0]
+                    self.author = line[2:]
                 elif line[1] == 't':
-                    self.title = line[2:0]
-            continue
+                    self.title = line[2:]
+                continue
         ## planet: [((x,y), radius, force, (randomdropinfo), color/text), ...] hopeigettoletyouknow
         ##randdrinf: (((nuthin, 40), (ubergun,10), (candy,50)), period)        beforetheylockmesillyarseaway
             line = line.split(';')
-            self.planets.append(line)
+            self.planets.append(planet(line))
+    def __str__(self):
+        line = 'Sistema ' + self.title + '.\n'
+        line += 'Made by ' + self.author + ' for ' + str(self.pl[0])
+        line += ' to ' + str(self.pl[1]) + ' players.\n'
+        line += '\nContains ' + str(len(self.planets)) + ' planets:\n\n'
+        line += 'POS\t\tSize\tSpawner\tGrav\n'
+        for plan in self.planets:
+            line += str(plan.vPos).ljust(16) + '\t'.join(map(str, [plan.radius, plan.spawn, plan.force])) + '\n'
+        return line
 
 ssyslist = list()
-ssyslist.append(ssyst(maps/blandarena.py))
+##hay que pillar los mapas dinamicamente ahora
+maplist = listdir('maps')
+for lvl in maplist:
+    ssyslist.append(ssyst('maps/' + lvl))
